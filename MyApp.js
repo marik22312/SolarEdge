064d4a8f1235491d7a8f3fdfe9365a9366edbb64
Asaaf Hadad <asaf147369@gmail.com>
2017-08-13 14:05:38 +0300

    it dosent work

--- MyApp.js
+++ MyApp.js
@@ -26,3 +26,36 @@ jQuery('img.svg').each(function(){
           }, 'xml');
 
       });
+      $.fn.RegisterTabBar = function (id) {
+          var $ele = $(this);
+          if ($ele.prop("tagName") && $ele.prop("tagName").toLowerCase() == "ul" && id != undefined) {
+              $ele.attr("data-style-id", id);
+              $("<style type='text/css'></style>").attr("id", id).appendTo($("body"));
+              $ele.find("li").on("click", function () {
+                  var $li = $(this),
+                      CurrentIndex = $ele.find(".active").removeClass("active").index(),
+                      ClickedIndex = $li.addClass("active").index();
+
+                  function SetStyle($ele, $li, IsToLeft) {
+                      var ID = $ele.attr("data-style-id");
+                      if (ID == undefined) return;
+                      if ($ele.width() <= 0) {
+                          setTimeout(function () { SetStyle($ele, $li, IsToLeft); }, 100);
+                          return;
+                      }
+
+                      $("style#" + ID).text(
+                          "ul[data-style-id='" + ID + "']:before { " +
+                              "left: " + $li.position().left + "px; " +
+                              "right: " + ($ele.width() - $li.position().left - $li.outerWidth()) + "px; " +
+                              "-webkit-transition: left " + (IsToLeft ? ".45s" : ".8s") + ", right " + (IsToLeft ? ".9s" : ".3s") + "; " +
+                              "transition: left " + (IsToLeft ? ".45s" : ".8s") + ", right " + (IsToLeft ? ".9s" : ".3s") + "; " +
+                          "} "
+                      );
+                  }
+                  SetStyle($ele, $li, ClickedIndex < CurrentIndex);
+              });
+          }
+          return $ele;
+      }
+      $(".slider").RegisterTabBar("slider1").find("li:first-child").click();
