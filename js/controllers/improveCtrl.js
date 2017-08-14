85c02f17c428ec4bdf6d22cbc652d0ee372d66f6
unknown <mark22312@gmail.com>
2017-08-14 13:43:57 +0300

    improve page without CSS

--- js/controllers/improveCtrl.js
+++ js/controllers/improveCtrl.js
@@ -1,3 +1,24 @@
 app.controller("improveCtrl", function ($scope) {
-    $scope.videoID = "atyvdC15HFA";
+    $scope.videos = [
+      {
+        videoID: "atyvdC15HFA", //moby - eveloving
+        value: "1"
+      },
+      {
+        videoID: "13EifDb4GYs", // moby - porcelain
+        value: "2"
+
+      },
+      {
+        videoID: "6A2V9Bu80J4", // moby - flower
+        value: "3"
+
+      },
+      {
+        videoID: "5wrwcEZ3Btw", // moby - in this world
+        value: "4"
+
+      },
+    ];
+    $scope.videoSwitch = "atyvdC15HFA"
 });
