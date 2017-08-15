1174534d98a46ddf42424443f836a37558ab3f78
unknown <mark22312@gmail.com>
2017-08-15 20:41:19 +0300

    commit

--- js/controllers/improveCtrl.js
+++ js/controllers/improveCtrl.js
@@ -1,31 +1,24 @@
-app.controller("improveCtrl", function ($scope) {
+app.controller("improveCtrl", function ($scope, $filter) {
     $scope.videos = [
       {
         videoID: "atyvdC15HFA", //moby - eveloving
-        value: "1",
         name: "Moby - Everloving",
         description: "Description Goes Here",
       },
       {
         videoID: "13EifDb4GYs", // moby - porcelain
-        value: "2",
         name: "Moby - Porcelain",
         description: "Moby - Porcelain",
-
       },
       {
         videoID: "6A2V9Bu80J4", // moby - flower
-        value: "3",
         name: "Moby - Flower",
         description: "Description Goes Here",
-
       },
       {
         videoID: "5wrwcEZ3Btw", // moby - in this world
-        value: "4",
         name: "Moby - in this world",
         description: "Description Goes Here",
-
       },
     ];
     $scope.videoSwitch = "atyvdC15HFA"
