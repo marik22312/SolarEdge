07d101376293511b8bde8f1ef48896342c304c0a
unknown <mark22312@gmail.com>
2017-08-15 22:06:15 +0300

    added video desc and name to selected video

--- js/controllers/improveCtrl.js
+++ js/controllers/improveCtrl.js
@@ -3,23 +3,22 @@ app.controller("improveCtrl", function ($scope, $filter) {
       {
         videoID: "atyvdC15HFA", //moby - eveloving
         name: "Moby - Everloving",
-        description: "Description Goes Here",
+        desc: "Description Goes Here",
       },
       {
         videoID: "13EifDb4GYs", // moby - porcelain
         name: "Moby - Porcelain",
-        description: "Moby - Porcelain",
+        desc: "Moby - Porcelain",
       },
       {
         videoID: "6A2V9Bu80J4", // moby - flower
         name: "Moby - Flower",
-        description: "Description Goes Here",
+        desc: "Description Goes Here",
       },
       {
         videoID: "5wrwcEZ3Btw", // moby - in this world
         name: "Moby - in this world",
-        description: "Description Goes Here",
+        desc: "Description Goes Here",
       },
     ];
-    $scope.videoSwitch = "atyvdC15HFA"
 });
