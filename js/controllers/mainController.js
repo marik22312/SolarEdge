c918f8fc30189c4230e1bebcb121ac6bc14ea10f
unknown <mark22312@gmail.com>
2017-08-17 17:23:18 +0300

    renamed controllers for route

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -11,7 +11,7 @@ app.config(function($routeProvider) {
         controller: "improveCtrl",
     })
     .when("/increase", {
-        templateUrl : "./templates/improve.html",
+        templateUrl : "./templates/player.html",
         controller: "increaseCtrl",
     })
     .when("/speed", {
