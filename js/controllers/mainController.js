0d601ff54da10564ddb75f5d9935cfd7c8ec0a46
unknown <mark22312@gmail.com>
2017-08-18 14:33:26 +0300

    added readme

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,30 +1,37 @@
+  /**
+  * Simple Video selector
+  * Version: 1.0
+  * Developed By Marik Sh & Asaf Hadad
+  * Main Controller
+  */
+
 var app = angular.module('solarEdge', ['ngRoute','youtube-embed']);
 // Router Config
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
         templateUrl : "./templates/player.html",
-        controller: "improveCtrl",
+        controller: "improveCtrl", //Category Controller
     })
     .when("/improve", {
         templateUrl : "./templates/player.html",
-        controller: "improveCtrl",
+        controller: "improveCtrl", //Category Controller
     })
     .when("/increase", {
         templateUrl : "./templates/player.html",
-        controller: "increaseCtrl",
+        controller: "increaseCtrl", //Category Controller
     })
     .when("/speed", {
         templateUrl : "./templates/player.html",
-        controller: "speedCtrl",
+        controller: "speedCtrl", //Category Controller
     })
     .when("/save", {
         templateUrl : "./templates/player.html",
-        controller: "saveCtrl",
+        controller: "saveCtrl", //Category Controller
     })
     .when("/manage", {
         templateUrl : "./templates/player.html",
-        controller: "manageCtrl",
+        controller: "manageCtrl", //Category Controller
     })
     .otherwise({
       // default page
--- js/controllers/saveCtrl.js
+++ js/controllers/saveCtrl.js
@@ -1,49 +1,56 @@
+/**
+* SolarEdge Video Selector
+* Version: 1.0
+* Developed By Marik Sh & Asaf Hadad
+* Controler For: Save Category
+*/
+
 app.controller("saveCtrl", function ($scope) {
     $scope.videos = [
       {
-        videoID: "cJpB_AEZf6U",
-        name: "Bob Dylan - Knockin' On Heaven's Door",
-        desc: "Description Goes Here",
+        videoID: "cJpB_AEZf6U", //Youtube Video ID
+        name: "Bob Dylan - Knockin' On Heaven's Door", // Video Title
+        desc: "Description Goes Here", // Video Description
       },
       {
-        videoID: "L9EKqQWPjyo",
-        name: "Bob Dylan - Things Have Changed",
-        desc: "Moby - Porcelain",
+        videoID: "L9EKqQWPjyo", //Youtube Video ID
+        name: "Bob Dylan - Things Have Changed", // Video Title
+        desc: "Moby - Porcelain", // Video Description
       },
       {
-        videoID: "YwSZvHqf9qM",
-        name: "Bob Dylan - Tangled Up In Blue",
-        desc: "Description Goes Here",
+        videoID: "YwSZvHqf9qM", //Youtube Video ID
+        name: "Bob Dylan - Tangled Up In Blue", // Video Title
+        desc: "Description Goes Here", // Video Description
       },
       {
-        videoID: "Go2jbER0wk0",
-        name: "Bob Dylan - Lay Lady Lay",
-        desc: "Description Goes Here",
+        videoID: "Go2jbER0wk0", //Youtube Video ID
+        name: "Bob Dylan - Lay Lady Lay", // Video Title
+        desc: "Description Goes Here", // Video Description
       },
       {
-        videoID: "yZPh3hpxLKs",
-        name: "Bob Dylan - Don't Think Twice It's Alright",
-        desc: "Description Goes Here",
+        videoID: "yZPh3hpxLKs", //Youtube Video ID
+        name: "Bob Dylan - Don't Think Twice It's Alright", // Video Title
+        desc: "Description Goes Here", // Video Description
       },
       {
-        videoID: "BzanOzyqgas",
-        name: "Bob Dylan - All Along the Watchtower",
-        desc: "Description Goes Here",
+        videoID: "BzanOzyqgas", //Youtube Video ID
+        name: "Bob Dylan - All Along the Watchtower", // Video Title
+        desc: "Description Goes Here", // Video Description
       },
       {
-        videoID: "nBgkw06JhFA",
-        name: "Bob Dylan - I Shall Be Released",
-        desc: "Description Goes Here",
+        videoID: "nBgkw06JhFA", //Youtube Video ID
+        name: "Bob Dylan - I Shall Be Released", // Video Title
+        desc: "Description Goes Here", // Video Description
       },
       {
-        videoID: "3l4nVByCL44",
-        name: "Blowin in The Wind - Bob Dylan",
-        desc: "Description Goes Here",
+        videoID: "3l4nVByCL44", //Youtube Video ID
+        name: "Blowin in The Wind - Bob Dylan", // Video Title
+        desc: "Description Goes Here", // Video Description
       },
       {
-        videoID: "T5al0HmR4to",
-        name: "A Hard Rain's A-Gonna Fall",
-        desc: "Description Goes Here",
+        videoID: "T5al0HmR4to", //Youtube Video ID
+        name: "A Hard Rain's A-Gonna Fall", // Video Title
+        desc: "Description Goes Here", // Video Description
       },
     ];
 });
